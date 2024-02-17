55e1c9b5e04e5a536c6ad3ec91499e20ad8daa69
Nathan <nslee333@gmail.com>
2024-02-17 11:15:35 -0800

    formatting

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 const selection = document.getElementById("select");
 
 selection.addEventListener("change", function () {
-  let contact = document.getElementById("contact")
-  let sponsor = document.getElementById("sponsor")
-  let talk = document.getElementById("talk")
+  let contact = document.getElementById("contact");
+  let sponsor = document.getElementById("sponsor");
+  let talk = document.getElementById("talk");
 
-  contact.setAttribute("hidden", "hidden")
-  sponsor.setAttribute("hidden", "hidden")
-  talk.setAttribute("hidden", "hidden")
-  
+  contact.setAttribute("hidden", "hidden");
+  sponsor.setAttribute("hidden", "hidden");
+  talk.setAttribute("hidden", "hidden");
+
   if (selection.value === "contact") {
-    contact.removeAttribute("hidden")
+    contact.removeAttribute("hidden");
 
   } else if (selection.value === "sponsor") {
-    sponsor.removeAttribute("hidden")
-
+    sponsor.removeAttribute("hidden");
+    
   } else if (selection.value === "talk") {
-    talk.removeAttribute("hidden")
+    talk.removeAttribute("hidden");
   }
 });
