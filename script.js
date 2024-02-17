80ac624cd1ce6a22ef3f716c839d432dd5e96f84
Nathan <nslee333@gmail.com>
2024-02-17 11:01:39 -0800

    Added script for form selection

---
+++ script.js
@@ -0,0 +1,21 @@
+const selection = document.getElementById("select");
+
+selection.addEventListener("change", function () {
+  let contact = document.getElementById("contact")
+  let sponsor = document.getElementById("sponsor")
+  let talk = document.getElementById("talk")
+
+  contact.setAttribute("hidden", "hidden")
+  sponsor.setAttribute("hidden", "hidden")
+  talk.setAttribute("hidden", "hidden")
+  
+  if (selection.value === "contact") {
+    contact.removeAttribute("hidden")
+
+  } else if (selection.value === "sponsor") {
+    sponsor.removeAttribute("hidden")
+
+  } else if (selection.value === "talk") {
+    talk.removeAttribute("hidden")
+  }
+});
